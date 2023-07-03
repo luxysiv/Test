0c2c7dc9b6f1330f102fd3f117bc8b8a44cac031
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-07-03 16:37:48 +0700

    update

---
+++ make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+input_file="domain.txt"
+output_file="hosts.txt"
+while IFS= read -r line; do
+   host_line="0.0.0.0 $line"
+   echo "$host_line" >> "$output_file"
+done < "$input_file" | sort
