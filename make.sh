46766121829a60baa01a7d0f0ac9102b7e7dfbad
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-07-03 19:25:01 +0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
+# Tên tệp văn bản gốc
 input_file="domain.txt"
+# Tên tệp văn bản mới
 output_file="hosts.txt"
-sort "$input_file" | uniq | while IFS= read -r line; do
-   host_line="0.0.0.0 $line"
-   echo "$host_line" >> "$output_file"
-done < "$input_file" | sort
+
+# Mảng để lưu trữ các dòng duy nhất
+unique_lines=()
+
+# Đọc từng dòng trong tệp văn bản và loại bỏ dòng trùng
+while IFS= read -r line; do
+   # Kiểm tra xem dòng đã có trong mảng unique_lines chưa
+   if ! [[ "${unique_lines[*]}" =~ (^|[[:space:]])"$line"($|[[:space:]]) ]]; then
+      # Lưu dòng vào mảng unique_lines và ghi vào tệp văn bản mới
+      unique_lines+=("$line")
+      modified_line="0.0.0.0 $line"
+      echo "$modified_line" >> "$output_file"
+   fi
+done < <(sort "$input_file")
